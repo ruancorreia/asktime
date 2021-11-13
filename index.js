dda15273e17197aec18fa741c6655e05260e2894
ruancorreia <correia.ruan@gmail.com>
2021-11-13 17:14:46 -0300

    commit

--- index.js
+++ index.js
@@ -4,28 +4,7 @@ const app = express();
 const path = require("path");
 const routes = require("./src/routes/index")
 const bodyParser = require("body-parser");
-const connection = require("./src/database/database");
-const Pergunta = require("./src/database/Pergunta");
 
-/* 
-Conexao ao banco de dados
-
-1- conexao
-2- autenticacao
-3- condicional positiva
-4- condicional negativa
-*/
-   
-    connection
-    .authenticate()
-        .then(()=>{
-            console.log("Sucessful Connection!");
-        })
-        .catch((msgErro)=>{
-            console.log(msgErro) 
-        })
-
-//porta
 const port = 3333;
 
 app.set("view engine", "ejs");
@@ -43,18 +22,9 @@ app.use(express.static(path.resolve('src', 'public')));
 app.use(routes);
 
 app.post("/salvarpergunta", (req, res)=>{
-    
-    //recebo os dados do formulario e armazeno em variaveis 
     var titulo = req.body.titulo;
     var descricao = req.body.descricao;
-
-    //depois eu salvo esse formulario no banco de dados, passando os dados que vem do formulario 
-    Pergunta.create({
-        titulo:titulo,
-        descricao:descricao // se tudo ocorrer bem, vai pro .then e redireciona pra pagina inicial
-    }).then(()=>{
-        res.redirect("/")
-    });
+    res.send("Formulario enviado!" + "Titulo: "+ titulo + " Descrição: " + descricao)
 });
 
 
